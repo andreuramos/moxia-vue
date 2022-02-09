21e57de38e87949549a493a781fc7f2f90501531
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2022-02-09 10:01:14 +0100

    add missing files :sweat:

---
+++ src/components/Landing.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="text-container">
+    <p class="main-text">
+      {{ text }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Landing",
+  data() {
+    return {
+      text: "Això era i no era una cervesa mallorquina de bon de veres."
+    }
+  }
+}
+</script>
+
+<style>
+@font-face {
+  font-family: "Bourbon";
+  src: url("../assets/BourbonSt-Regular.ttf");
+}
+
+.text-container {
+  margin: 15%;
+}
+
+.main-text {
+  font-family: Bourbon;
+  text-transform: uppercase;
+  font-size: xxx-large;
+  color: var(--dark-color);
+  text-align: center;
+}
+</style>
