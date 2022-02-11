b5443a8dc4af9c139dd89ff8242b6e29d420c4de
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2022-02-11 20:27:31 +0100

    added overage dialog

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <Dialog
+        message="Uep aqueix! Que ets major d'edat per agafar una moixa?"
+        ok-button="Vés! Posa'men 3"
+        ko-button="Batua, idoi no"
+    />
     <Header/>
     <Landing/>
   </div>
@@ -8,11 +13,12 @@
 <script>
 import Header from "./components/Header";
 import Landing from "@/components/Landing";
+import Dialog from "@/components/Dialog";
 
 export default {
   name: 'App',
   components: {
-    Header, Landing
+    Header, Landing, Dialog
   }
 }
 </script>
---
+++ src/components/Dialog.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="overlay">
+    <div class="dialog">
+      <div class="content">
+        <span>{{ message }}</span>
+      </div>
+      <div class="dialog-buttons">
+        <a class="button ok">{{ okButton }}</a>
+        <a v-if="koButton" class="button ko">{{ koButton }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dialog",
+  props: ["message", "okButton", "koButton"]
+}
+</script>
+
+<style scoped>
+.overlay {
+  background-color: rgba(2,2,2,60%);
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+
+.dialog {
+  position: relative;
+  margin: 0 auto;
+  width: 80%;
+  background-color: var(--main-color);
+  top: 50%;
+  transform: translateY(-50%);
+  font-family: Moderat;
+  font-size: larger;
+}
+
+.content {
+  display: block;
+  padding: 2em;
+}
+
+.dialog-buttons {
+  display: block;
+  padding: 1em;
+}
+
+.button {
+  display: inline-block;
+  margin: 0.5em;
+}
+
+@media only screen and (min-width: 700px) {
+  .dialog {
+    width: 30%;
+  }
+}
+</style>
