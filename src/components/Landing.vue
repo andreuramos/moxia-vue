d885dc03f3daed4ed13bec02abd05dde011077b4
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2022-02-20 13:58:27 +0100

    dolentia seal

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -2,6 +2,7 @@
   <section>
     <div class="container">
       <img id="landing-img" src="../assets/landing-sm.svg" alt="Cervesa Artesana de Mallorca"/>
+      <img id="dolentia-seal" src="../assets/feta-dolentia.svg"/>
     </div>
   </section>
 </template>
@@ -23,9 +24,21 @@ export default {
   margin: 5%;
 }
 
+#dolentia-seal {
+  position: fixed;
+  display: block;
+  top: 40%;
+  right:0%;
+}
+
 @media only screen and (min-width: 700px){
   #landing-img {
     content: url("../assets/landing-lg.svg");
+  }
+
+  #dolentia-seal {
+    top: 65%;
+    right: 15%;
   }
 }
 </style>
