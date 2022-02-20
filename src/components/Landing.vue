88bdbc95d0d24ddd81f459179446fa32df1335f9
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2022-02-20 13:53:41 +0100

    refactored landing section to be implemented as an img

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,22 +1,9 @@
 <template>
-  <div class="container">
-    <div class="text-container">
-      <p>
-        <span class="main-text outline">Això era i no era</span>
-      </p>
-      <p>
-        <span class="main-text outline">una</span>
-        <span class="main-text">&nbsp;cervesa&nbsp;</span>
-        <span class="main-text outline">mallorquina</span>
-      </p>
-      <p>
-        <span class="main-text outline">de bon de veres</span>
-      </p>
-      <p>
-        <span class="main-text">-moixa-</span>
-      </p>
+  <section>
+    <div class="container">
+      <img id="landing-img" src="../assets/landing-sm.svg" alt="Cervesa Artesana de Mallorca"/>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -30,27 +17,15 @@ export default {
   width: 100%;
 }
 
-.text-container {
-  margin: 5% 15%;
-  text-align: center;
-}
-
-.main-text {
-  display: inline-block;
-  font-family: Bourbon;
-  text-transform: uppercase;
-  font-size: 3em;
-  color: var(--dark-color);
-}
-
-.outline {
-  color: rgba(0,0,0,0%);
-  -webkit-text-stroke: 1px black;
+#landing-img {
+  content:url("../assets/landing-sm.svg");
+  width: 90%;
+  margin: 5%;
 }
 
 @media only screen and (min-width: 700px){
-  .main-text {
-    font-size: 8em;
+  #landing-img {
+    content: url("../assets/landing-lg.svg");
   }
 }
 </style>
